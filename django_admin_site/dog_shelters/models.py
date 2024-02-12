6d2fbf3e3aff440df8d7599fdcbf049fe554228f
pedrosd3 <pedro.dasilva.7080@gmail.com>
2024-02-12 13:51:01 -0300

    Django admin module

---
+++ django_admin_site/dog_shelters/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Shelter(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Dog(models.Model):
+    shelter = models.ForeignKey(Shelter, on_delete=models.PROTECT)
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=100)
+    intake_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
